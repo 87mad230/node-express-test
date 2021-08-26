b91d8040b3aded94408b2838af1c6dded4ccb45d
87mad230 <ghost-man230@outlook.com>
2021-08-26 06:20:18 -0700

    :)

--- myApp.js
+++ myApp.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html");
-})
+
 app.get('/json', (req,res) => {
-    res.json({"message": "Hello json"});
+    const style = process.env.MESSAGE_STYLE;
+    if (style === "uppercase") 
+        res.json({"message": "HELLO JSON"});
+    else {
+        res.json({"message": "Hello json"});
+    }
 })
-let static1 = express.static(__dirname + "/public");
-app.use("/public",static1);
+
 
 
 
