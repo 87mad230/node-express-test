44b58daf37841fcd5f472c948524a267c14d03bd
87mad230 <ghost-man230@outlook.com>
2021-08-26 05:59:18 -0700

    :)

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ console.log("Hello World");
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
+app.get('/json', (req,res) => {
+    res.json({"message": "Hello json"});
+})
 let static1 = express.static(__dirname + "/public");
 app.use("/public",static1);
 
