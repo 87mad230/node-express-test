83b40cff15cd2fb9fe01a6b00206b323ffac69a3
87mad230 <ghost-man230@outlook.com>
2021-08-27 04:55:24 -0700

    :)

--- myApp.js
+++ myApp.js
@@ -2,13 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:true}));
+//app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+"/views/index.html");
 })
 
-app.post('/',)
 
 
 
