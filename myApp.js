ee5d80d26d2cd779fdfb2258aeb137da696fc779
87mad230 <ghost-man230@outlook.com>
2021-08-27 05:11:05 -0700

    :)

--- myApp.js
+++ myApp.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-//app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/public',express.static(__dirname+'/public'));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+"/views/index.html");
 })
-
+app.post('/name', (req,res) => {
+    res.json({'name':`${req.body.first} ${req.body.last}`});
+})
 
 
 
