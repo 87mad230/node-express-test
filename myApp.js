95078b6d9fe608ad530567db207352cab4451f21
87mad230 <ghost-man230@outlook.com>
2021-08-27 02:30:24 -0700

    new commit

--- myApp.js
+++ myApp.js
@@ -4,11 +4,7 @@ var app = express();
 
 app.get('/json', (req,res) => {
     const style = process.env.MESSAGE_STYLE;
-    if (style === "uppercase") 
-        res.json({"message": "HELLO JSON"});
-    else {
-        res.json({"message": "Hello json"});
-    }
+    res.send(style);
 })
 
 
